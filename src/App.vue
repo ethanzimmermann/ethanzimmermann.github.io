10e461d59483962a99394445d6c488cdb2b95010
Ethan <ethan.a.zimmermann@gmail.com>
2021-01-12 11:58:12 -0600

    chore: deployment test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to your doom"/>
+    <HelloWorld msg="Welcome to my site?"/>
   </div>
 </template>
 
