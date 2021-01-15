51d903826d5356140735b8d64e2037bf1b2511cd
Ethan <ethan.a.zimmermann@gmail.com>
2021-01-15 17:12:02 -0600

    fix: updating navbar links and text

--- src/App.vue
+++ src/App.vue
@@ -31,28 +31,17 @@ export default {
         ariaLabelMainNav: "Main Navigation",
         tooltipAnimationType: "shift-away",
         tooltipPlacement: "bottom",
-        menuOptionsLeft: [
+        menuOptionsRight: [
           {
             type: "link",
-            text: "Why Dunder Mifflin",
-            arrowColor: "#659CC8",
+            text: "Coming Soon",
+            arrowColor: "black",
             subMenuOptions: [
               {
-                isLinkAction: true,
                 type: "link",
-                text: "About",
-                subText: "Stupid corporate wet blankets. Like booze ever killed anyone.",
-                path: { name: "about" },
-                iconLeft: '<i class="fa fa-star fa-fw"></i>'
-              },
-              {
-                type: "hr",
-              },
-              {
-                type: "link",
-                text: "Locations",
-                subText: "You're a presentation tool!",
-                path: { name: "locations" },
+                text: "Personal Interests",
+                subText: "Activities and topics I enjoy pursuing and researching",
+                path: { name: "" },
                 arrowColor: "#659CC8",
               },
               {
@@ -60,56 +49,34 @@ export default {
               },
               {
                 type: "link",
-                text: "Blog",
-                subText: "I enjoy having breakfast in bed. I like waking up to the smell of bacon. Sue me.",
-                path: { name: "blog" }
-              },
-            ]
-          },
-          {
-            type: "link",
-            text: "Contact",
-            subMenuOptions: [
-              {
-                type: "link",
-                text: "Customer Service",
-                path: { name: "customer-service" },
-              },
-              {
-                type: "link",
-                text: "Accounting",
-                path: { name: "accounting" },
+                text: "Untitled Animation Project",
+                subText: "",
+                path: { name: "" },
+                arrowColor: "#659CC8",
               },
               {
                 type: "hr",
               },
               {
                 type: "link",
-                text: "Reception",
-                path: { name: "reception"},
-                iconLeft: '<svg id="i-telephone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M3 12 C3 5 10 5 16 5 22 5 29 5 29 12 29 20 22 11 22 11 L10 11 C10 11 3 20 3 12 Z M11 14 C11 14 6 19 6 28 L26 28 C26 19 21 14 21 14 L11 14 Z" /> <circle cx="16" cy="21" r="4" /> </svg>',
-              },
+                text: "Graphing Tool Demos",
+                subText: "I will be posting data displayed in a variety of different vue-friendly graphing packages and comparing them based on functionality, extensibility, and ease of use.",
+                path: { name: "" },
+                arrowColor: "#659CC8",
+              }
             ]
+          }
+        ],
+        menuOptionsLeft: [
+          {
+            type: "link",
+            text: "Home",
+            path: { name: "" },
           },
           {
             type: "link",
-            text: "Pricing",
-            path: { name: "pricing"},
-            iconRight: '<i class="fa fa-long-arrow-right fa-fw"></i>',
-          },
-        ],
-        menuOptionsRight: [
-          {
-            type: "button",
-            text: "Signup",
-            path: { name: "signup" },
-            class: "button-red"
-          },
-          {
-            type: "button",
-            text: "Login",
-            path: { name: "login" },
-            iconRight: '<svg id="i-arrow-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M22 6 L30 16 22 26 M30 16 L2 16" /> </svg>'
+            text: "Professional Work",
+            path: { name: "" },
           }
         ]
       }
